ce47eb47c0f2b7262125941f3659251b01b689e8
yT094 <1748780248@qq.com>
2022-02-20 20:45:50 +0800

    feat: swiper feature

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,42 +1,51 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav">
-      <div slot="center">购物街</div>
-    </nav-bar>
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+
+    <swiper>
+      <swiper-item v-for="item in banners" :key="item.id">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </swiper-item>
+    </swiper>
   </div>
 </template>
 
 <script>
-  import NavBar from 'components/common/navbar/NavBar'
-  import { getHomeMultidata } from "network/home"
+import NavBar from "components/common/navbar/NavBar";
+import { getHomeMultidata } from "network/home";
+import { Swiper, SwiperItem } from "components/common/swiper";
 
-  export default {
-    name: "Home",
-    components: {
-      NavBar
-    },
-    data() {
-      return {
-        banners: [],
-        recommends: []
-      }
-    },
-    // 组件创建完成就发送网络请求
-    created() {
-      // 1.请求多个数据（这是一个异步操作）
-      getHomeMultidata().then( res => {
-        // console.log(res);
-        // this.result = res;
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
-      })
-    }
-  }
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+    Swiper,
+    SwiperItem,
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+  // 组件创建完成就发送网络请求
+  created() {
+    // 1.请求多个数据（这是一个异步操作）
+    getHomeMultidata().then((res) => {
+      // console.log(res);
+      // this.result = res;
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+  },
+};
 </script>
 
 <style scoped>
-  .home-nav {
-    background-color: var(--color-tint);
-    color: #fff;
-  }
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
