1d0fcf95ff1b026a852cdb00644150b4cbe5dfe5
yT094 <1748780248@qq.com>
2022-02-20 18:04:20 +0800

    feat: tabbar, getHomeMultidata

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  productionSourceMap: false,
+  configureWebpack: {
+    resolve: {
+      // 配置路径别名
+      alias: {
+        assets: "@/assets",
+        common: "@/common",
+        components: "@/components",
+        network: "@/network",
+        views: "@/views"
+      }
+    }
+  }
+};
