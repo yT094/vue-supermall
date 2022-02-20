d07e3c56f0f1e9d15d576f98492d032c0ef4c058
yT094 <1748780248@qq.com>
2022-02-20 21:02:33 +0800

    refactor: refactor HomeSwiper

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-
-    <swiper>
-      <swiper-item v-for="item in banners" :key="item.id">
-        <a :href="item.link">
-          <img :src="item.image" alt="">
-        </a>
-      </swiper-item>
-    </swiper>
+    <home-swiper :banners="banners"/>
   </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar";
-import { getHomeMultidata } from "network/home";
-import { Swiper, SwiperItem } from "components/common/swiper";
+import NavBar from "components/common/navbar/NavBar"
+import HomeSwiper from "./childComps/HomeSwiper"
+
+import { getHomeMultidata } from "network/home"
+
 
 export default {
   name: "Home",
   components: {
     NavBar,
-    Swiper,
-    SwiperItem,
+    HomeSwiper
   },
   data() {
     return {
