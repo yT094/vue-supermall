1abe0cf0f417723c7d7e2886cce24f6408943054
yT094 <1748780248@qq.com>
2022-02-21 12:38:46 +0800

    feat: add Feature

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,6 +3,7 @@
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>
+    <feature-view></feature-view>
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 import NavBar from "components/common/navbar/NavBar"
 import HomeSwiper from "./childComps/HomeSwiper"
 import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
 
 import { getHomeMultidata } from "network/home"
 
@@ -19,7 +21,8 @@ export default {
   components: {
     NavBar,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView
   },
   data() {
     return {
