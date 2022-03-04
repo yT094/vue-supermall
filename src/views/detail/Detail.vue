7004674c4090e79029a81326d037759be97786a3
yT094 <1748780248@qq.com>
2022-03-04 22:06:55 +0800

    feat: add detail feature

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>详情页
+    <div>{{iid}}</div>
+  </div>
+  
+</template>
+
+<script>
+  export default {
+    name: "Detail",
+    data() {
+      return {
+        iid: null
+      }
+    },
+    created() {
+      this.iid = this.$route.params.iid
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
