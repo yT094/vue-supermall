96d6acd381d7d593ce8dd0a92d78bf0fae260d55
yT094 <1748780248@qq.com>
2022-03-05 15:10:50 +0800

    feat: add Scroll in Detail

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,27 +1,40 @@
 <template>
   <div id="detail">
-    <detail-nav-bar/>
-    <detail-swiper :top-images="topImages"/>
+    <detail-nav-bar class="detail-nav"/>
+    <scroll class="content">
+      <detail-swiper :top-images="topImages"/>
+      <detail-base-info :goods="goods"/>
+      <detail-shop-info :shop="shop"/>
+    </scroll>
   </div>
 </template>
 
 <script>
   import DetailNavBar from "./childComps/DetailNavBar"
   import DetailSwiper from "./childComps/DetailSwiper"
+  import DetailBaseInfo from "./childComps/DetailBaseInfo"
+  import DetailShopInfo from "./childComps/DetailShopInfo"
   
-  import { getDetail } from "network/detail"
+  import Scroll from "components/common/scroll/Scroll"
+
+  import { getDetail, Goods, Shop } from "network/detail"
 
   export default {
     name: "Detail",
     data() {
       return {
         iid: null,
-        topImages: []
+        topImages: [],
+        goods: {},
+        Shop: {},
       }
     },
     components: {
       DetailNavBar,
-      DetailSwiper
+      DetailSwiper,
+      DetailBaseInfo,
+      DetailShopInfo,
+      Scroll
     },
     created() {
       // 1.保存传入的 iid
@@ -29,13 +42,35 @@
 
       // 2.根据 iid 请求详情数据
       getDetail(this.iid).then( res => {
+        const data = res.result
         // 1.获取顶部轮播图的数据
-        this.topImages = res.result.itemInfo.topImages
+        this.topImages = data.itemInfo.topImages
+
+        // 2.获取商品信息
+        this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+
+        // 3.获取店铺信息
+        this.shop = new Shop(data.shopInfo)
       })
     }
   }
 </script>
 
 <style scoped>
+  #detail {
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+    height: 100vh;
+  }
 
+  .detail-nav {
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+  }
+
+  .content {
+    height: calc(100% - 44px)
+  }
 </style>
