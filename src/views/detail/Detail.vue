bb86f1120a18d67883287f8b0945dbc5a1fc2b89
yT094 <1748780248@qq.com>
2022-03-05 09:29:44 +0800

    feat: add DetailNavBar in Detail

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,17 +1,19 @@
 <template>
-  <div>详情页
-    <div>{{iid}}</div>
-  </div>
-  
+  <detail-nav-bar/>
 </template>
 
 <script>
+  import DetailNavBar from "./childComps/DetailNavBar"
+
   export default {
     name: "Detail",
     data() {
       return {
         iid: null
       }
+    },
+    components: {
+      DetailNavBar
     },
     created() {
       this.iid = this.$route.params.iid
