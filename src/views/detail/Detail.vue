4e0163f627b4ce3618764d61dfa6f089b96baa40
yT094 <1748780248@qq.com>
2022-03-05 11:24:30 +0800

    feat: add topImages in Detail

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,22 +1,37 @@
 <template>
-  <detail-nav-bar/>
+  <div id="detail">
+    <detail-nav-bar/>
+    <detail-swiper :top-images="topImages"/>
+  </div>
 </template>
 
 <script>
   import DetailNavBar from "./childComps/DetailNavBar"
+  import DetailSwiper from "./childComps/DetailSwiper"
+  
+  import { getDetail } from "network/detail"
 
   export default {
     name: "Detail",
     data() {
       return {
-        iid: null
+        iid: null,
+        topImages: []
       }
     },
     components: {
-      DetailNavBar
+      DetailNavBar,
+      DetailSwiper
     },
     created() {
+      // 1.保存传入的 iid
       this.iid = this.$route.params.iid
+
+      // 2.根据 iid 请求详情数据
+      getDetail(this.iid).then( res => {
+        // 1.获取顶部轮播图的数据
+        this.topImages = res.result.itemInfo.topImages
+      })
     }
   }
 </script>
