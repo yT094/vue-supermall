eb576c4d462c42e805af5be1428e012658e948a2
yT094 <1748780248@qq.com>
2022-03-06 11:18:06 +0800

    feat: add recommend in Detail

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -8,6 +8,7 @@
       <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
       <detail-param-info :param-info="paramInfo"/>
       <detail-comment-info :comment-info="commentInfo"/>
+      <goods-list :goods="recommendList"/>
     </scroll>
   </div>
 </template>
@@ -22,8 +23,9 @@
   import DetailCommentInfo from "./childComps/DetailCommentInfo"
   
   import Scroll from "components/common/scroll/Scroll"
+  import GoodsList from "components/content/goods/GoodsList"
 
-  import { getDetail, Goods, Shop, GoodsParam } from "network/detail"
+  import { getDetail, Goods, Shop, GoodsParam, getRecommend } from "network/detail"
 
   export default {
     name: "Detail",
@@ -36,6 +38,7 @@
         detailInfo: {},
         paramInfo: {},
         commentInfo: {},
+        recommendList: [],
       }
     },
     components: {
@@ -46,7 +49,8 @@
       DetailGoodsInfo,
       DetailParamInfo,
       DetailCommentInfo,
-      Scroll
+      Scroll,
+      GoodsList
     },
     created() {
       // 1.保存传入的 iid
@@ -74,6 +78,11 @@
         if (data.rate.cRate !== 0) {
           this.commentInfo = data.rate.list[0] || {}
         }
+      })
+
+      // 3.获取推荐的数据
+      getRecommend().then( res => {
+        this.recommendList = res.data.list
       })
     },
     methods: {
